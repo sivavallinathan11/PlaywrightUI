ed09ea083db1d4ce77a7402de98a8ce482629f5e
Gordon Massey <gmassey@discoveryparks.com.au>
2023-04-18 17:05:35 +0930

    Updated config and package

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -1,54 +1,26 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices } from '@playwright/test';
+// import type { PlaywrightTestConfig } from '@playwright/test';
+import { PlaywrightTestConfig, devices } from '@playwright/test';
 import { ResultsDirectory, TestScriptDirectory, XMLResultDirectory } from './tests/data/users';
+import dotenv from 'dotenv';
+import path from 'path';
+// Alternatively, read from "../my.env" file.
+dotenv.config({ path: path.resolve(__dirname, './env', 'test.env') });
 
-// // Set default xml result path.
-// var resultPath = XMLResultDirectory.RC3;
 
-// // Change result path based on assigned test script directory.
-// if(TestScriptDirectory.Path.includes('RC2')){
-//   resultPath = XMLResultDirectory.RC2;
-// }
-// if(TestScriptDirectory.Path.includes('RC4')){
-//   resultPath = XMLResultDirectory.RC4;
-// }
-// if(TestScriptDirectory.Path.includes('RC5')){
-//   resultPath = XMLResultDirectory.RC5;
-// }
-// else if(TestScriptDirectory.Path.toLowerCase().includes('temp-rc3')){
-//   resultPath = XMLResultDirectory.TempRC3;
-// }
-
-// // Set result path default to RC3.
-// var testResultPath = ResultsDirectory.RC3;
-
-// // Change result path based on assigned test script directory.
-// if(TestScriptDirectory.Path.includes('RC2')){
-//   resultPath = ResultsDirectory.RC2;
-// }
-// if(TestScriptDirectory.Path.includes('RC4')){
-//   testResultPath = ResultsDirectory.RC4;
-// }
-// if(TestScriptDirectory.Path.includes('RC5')){
-//   testResultPath = ResultsDirectory.RC5;
-// }
-// else if(TestScriptDirectory.Path.toLowerCase().includes('temp-rc3')){
-//   testResultPath = ResultsDirectory.TempRC3;
-// }
 
 const config: PlaywrightTestConfig = {
-  /* Maximum time one test can run for. */
-  // timeout: 900000,
-  // globalTimeout: 8000000,
+
   reporter: [['list'], ['junit', {outputFile: 'results.xml'}]],
   testDir: './tests',
   outputDir: './screenshots',
 
-  // outputDir: testResultPath,
-  workers: 2,
-  //testMatch: ["PW-4332-AddBetterOfferTo1stBookingwithNonMemberAndJoinGDayAndLessOfferTo2ndBookingwithMember.spec.ts"],
+  workers: process.env.CI ? 2 : undefined,
+
   use: {
-    //trace: 'on',
+
+    baseURL: process.env.ENV, // === '1' ? 'https://test.gdaynetwork.com.au/' : 'https://dev.gdaynetwork.com.au/',
+    screenshot: 'only-on-failure',
+    
   },
 
   /* Configure projects for major browsers */
@@ -68,47 +40,7 @@ const config: PlaywrightTestConfig = {
         channel: 'msedge',
       },
     }
-    /*{
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
-
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },*/
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
+    
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
